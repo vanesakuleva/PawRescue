1b8489029c0554732655332460ba3139e5d6a2d0
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-19 11:54:31 +0300

    update

--- PawRescue/common/urls.py
+++ PawRescue/common/urls.py
@@ -4,4 +4,5 @@ from PawRescue.common.views import IndexView
 
 urlpatterns = (
     path('', IndexView.as_view(), name='index'),
+    path('cataloguePets/', views.catalogue, name='catalogue Pets'),
 )
