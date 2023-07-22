e22507b3caac889a7c9bd815187ec63c28883455
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-22 18:00:28 +0300

    update

--- PawRescue/adoption/views.py
+++ PawRescue/adoption/views.py
@@ -1,14 +1,20 @@
-# from django.shortcuts import render, get_object_or_404
-# from django.views import generic as views
-# from PawRescue.adoption.models import Adoption
-#
-#
-# class AdoptionDetailView(views.DetailView):
-#     model = Adoption
-#     template_name = 'adoption/adoption_details.html'
-#     context_object_name = 'adoption'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['email'] = self.object.user.email
-#         return context
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic as views
+
+from PawRescue.adoption.forms import AdoptionForm
+from PawRescue.adoption.models import Adoption
+
+
+class AdoptionDetailView(views.CreateView):
+    model = Adoption
+    form_class = AdoptionForm
+    template_name = 'adoption/adoption_details.html'
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        pet_id = self.kwargs['pk']
+        form.instance.pet_id = pet_id
+        form.instance.user = self.request.user
+        form.instance.status = 'Pending'
+        return super().form_valid(form)
