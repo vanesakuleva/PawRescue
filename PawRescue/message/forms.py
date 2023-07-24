baf419d26c510c4952897b30004bce6614075a0c
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-24 09:25:40 +0300

    update

--- PawRescue/message/forms.py
+++ PawRescue/message/forms.py
@@ -5,4 +5,11 @@ from .models import Message
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
-        fields = ['recipient', 'subject', 'body']
+        exclude = ['recipient', 'sender', 'answered']
+
+    def __init__(self, *args, **kwargs):
+        original_subject = kwargs.pop('original_subject', None)
+        if original_subject:
+            initial_subject = f"Re: {original_subject}"
+            kwargs.setdefault('initial', {})['subject'] = initial_subject
+        super().__init__(*args, **kwargs)
--- PawRescue/message/urls.py
+++ PawRescue/message/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from PawRescue.message.views import CreateMessageView, message_sent_success
+from PawRescue.message.views import CreateMessageView, message_sent_success, inbox
 
 urlpatterns = [
-    path('create/', CreateMessageView.as_view(), name='inbox'),
-    path('/sent_success/<str:recipient>/', message_sent_success, name='message_sent_success'),
+    path('create/<int:pk>/', CreateMessageView.as_view(), name='create_message'),
+    path('sent_success/<str:recipient>/', message_sent_success, name='message_sent_success'),
+    path('inbox/', inbox, name='inbox'),
+    path('answer/<int:message_pk>/', CreateMessageView.as_view(), name='answer_message'),
 ]
