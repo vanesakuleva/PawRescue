93430d662ee36995444fd8ee533c1a8642d9e265
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-24 14:07:59 +0300

    update

--- PawRescue/adoption/admin.py
+++ PawRescue/adoption/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-from PawRescue.adoption.models import Adoption
+from PawRescue.adoption.models import Adoption, ApprovedAdoption
 
 
 @admin.register(Adoption)
 class AdoptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'pet', 'status')
-    list_filter = ('status',)
+    list_display = ('user', 'pet', 'status', 'approve_button')
+
+    def approve_button(self, obj):
+        if obj.status == 'Pending':
+            # Generate the URL for the approve_adoption view and add the is_approved parameter
+            approve_url = reverse('approve_adoption', args=[obj.id]) + '?is_approved=true'
+            return format_html('<a class="button" href="{}">Approve</a>', approve_url)
+        return '-'
+
+    approve_button.short_description = 'Approve Adoption'
