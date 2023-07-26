0917ed1265576d2f4c755a168d01114614280ffa
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-26 12:13:41 +0300

    update the code

--- PawRescue/adoption/views.py
+++ PawRescue/adoption/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views import generic as views, generic
 
