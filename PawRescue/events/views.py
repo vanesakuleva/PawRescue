df9deb18c6ba9a2492693a69698d76199cedde8e
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-30 20:16:39 +0300

    create views for Events app

--- PawRescue/events/views.py
+++ PawRescue/events/views.py
@@ -3,10 +3,9 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from PawRescue.events.forms import AdoptionEventForm
-from PawRescue.events.models import Hashtag
+from PawRescue.events.models import AdoptionEvent
 
 
-# Create your views here.
 class AdoptionEventCreateView(generic.FormView):
     template_name = 'events/create_adoption_event.html'
     form_class = AdoptionEventForm
@@ -14,24 +13,12 @@ class AdoptionEventCreateView(generic.FormView):
 
     def form_valid(self, form):
         event = form.save(commit=False)
-
-        # Save the event first to get its ID
         event.organizer = self.request.user
         event.save()
-
-        selected_hashtags = form.cleaned_data.get('hashtags', [])
-        custom_hashtags = form.cleaned_data.get('custom_hashtags', '').strip()
-
-        # Create and add the selected hashtags
-        for hashtag_name in selected_hashtags:
-            hashtag, _ = Hashtag.objects.get_or_create(name=hashtag_name)
-            event.hashtags.add(hashtag)
-
-        # Create and add the custom hashtags
-        if custom_hashtags:
-            custom_hashtag_list = [tag.strip() for tag in custom_hashtags.split(',')]
-            for custom_hashtag_name in custom_hashtag_list:
-                custom_hashtag, _ = Hashtag.objects.get_or_create(name=custom_hashtag_name)
-                event.hashtags.add(custom_hashtag)
-
         return super().form_valid(form)
+
+
+class EventCatalogView(generic.ListView):
+    model = AdoptionEvent
+    template_name = 'events/adoption_events.html'
+    context_object_name = 'events'
