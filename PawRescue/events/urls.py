0472b643cbeeb21534ddca4f46eb932526f93b53
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-07-30 20:15:53 +0300

    create path for the event catalog

--- PawRescue/events/urls.py
+++ PawRescue/events/urls.py
@@ -1,8 +1,9 @@
 from django import views
 from django.urls import path
 
-from PawRescue.events.views import AdoptionEventCreateView
+from PawRescue.events.views import AdoptionEventCreateView, EventCatalogView
 
 urlpatterns = [
-    path('create/', AdoptionEventCreateView.as_view(), name='create adoption event')
+    path('create/', AdoptionEventCreateView.as_view(), name='create adoption event'),
+    path('events/catalog/', EventCatalogView.as_view(), name='event catalog'),
 ]
