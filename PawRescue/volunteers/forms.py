490f6e4bc530e6821a96a5d78f76e14887390483
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-08-06 18:23:38 +0300

    add placeholders on the volunteer  forms

--- PawRescue/volunteers/forms.py
+++ PawRescue/volunteers/forms.py
@@ -16,5 +16,41 @@ class VolunteerApplicationForm(forms.ModelForm):
             'why_volunteer',
         ]
         widgets = {
-            'why_volunteer': forms.Textarea(attrs={'rows': 5}),
+            'full_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type your  full name',
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder': 'Type your Email'
+                }
+            ),
+            'contact_number': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type your phone number'
+                }
+            ),
+
+            'address': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type your full address',
+                }
+            ),
+            'skills': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type your skills'
+                }
+            ),
+            'availability': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type your free hours'
+                }
+            ),
+            'why_volunteer': forms.Textarea(
+                attrs={
+                    'placeholder': 'Tell us why you want to be a volunteer'
+                }
+            )
+
         }
