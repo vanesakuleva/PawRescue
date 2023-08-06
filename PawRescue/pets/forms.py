5bd0dd6f3dfdac65e277fcf40e58cd120c9850ed
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-08-06 21:14:55 +0300

    add placeholders on the Pets  forms

--- PawRescue/pets/forms.py
+++ PawRescue/pets/forms.py
@@ -7,3 +7,41 @@ class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         exclude = ['created_by']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type name of the pet'
+                }
+            ),
+            'other_pet_type': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type type of the pet'
+                }
+            ),
+            'breed': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type the breed if have any'
+                }
+            ),
+            'age': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type the age of the pet'
+                }
+            ),
+            'specifics_in_behavior':forms.Textarea(
+                attrs={
+                    'placeholder':'Type specifics behavior if have any'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'placeholder': 'Type description'
+                }
+            ),
+            'town': forms.TextInput(
+                attrs={
+                    'placeholder': 'Type the town were is the pet'
+                }
+            )
+
+        }
