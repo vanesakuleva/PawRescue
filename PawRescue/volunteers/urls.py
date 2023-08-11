766c860981aab63c7d96acf324156ce309fa326f
Vanesa Kuleva <vanesakalinovakuleva1603@gmail.com>
2023-08-11 22:29:38 +0300

    clear wrong code

--- PawRescue/volunteers/urls.py
+++ PawRescue/volunteers/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from PawRescue.volunteers.views import VolunteerApplicationView, ThankYouView, approve_volunteer_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('volunteer_application/', VolunteerApplicationView.as_view(), name='volunteer application'),
     path('thank_you/', ThankYouView.as_view(), name='thank you'),
     path('approve_volunteer/<int:pk>/', approve_volunteer_view, name='approve_volunteer'),
